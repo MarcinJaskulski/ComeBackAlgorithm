c6e64eecbef799303de999de2eed8b60c4134984
Safeguard14 <35404422+Safeguard14@users.noreply.github.com>
2018-05-22 01:50:12 +0200

    Finish

--- AlgorytmyZPowracaniem/Program.cs
+++ AlgorytmyZPowracaniem/Program.cs
@@ -15,10 +15,10 @@ namespace AlgorytmyZPowracaniem
             for (int i = 1; i < n; i++) Tab[i] = 0;
             Random rand = new Random();
 
-            for (int i = 0; i < n-1; i++) // pętla generująca łańczusze początkowy -> Cykl Hamiltona
-            {    
+            for (int i = 0; i < n - 1; i++) // pętla generująca łańczusze początkowy -> Cykl Hamiltona
+            {
                 x = rand.Next(0, n); // losowanie kolumny
-                if(Tab[x] == 0)
+                if (Tab[x] == 0)
                 {
                     adjacencyMatrix[y, x] = 1;  // Moment połączenia
                     adjacencyMatrix[x, y] = 1;  // Połączenie lustrzane
@@ -81,7 +81,7 @@ namespace AlgorytmyZPowracaniem
             //    Console.Write("\n");
             //}
             //Console.Write("\n");
-            
+
         }
 
         static void TransformToCommingList(ref int[,] adjacencyMatrix, ref List<int>[] commingList, int n) // zamiana na listę następników
@@ -109,55 +109,69 @@ namespace AlgorytmyZPowracaniem
             {
                 commingList[node].Remove(item);
                 commingList[item].Remove(node); // usuwanie krawędzi
-                   
+
                 FindEuler(item, ref commingList, ref solution); // wywołanie Eulera dla kolejnego elementu
 
                 item = commingList[node].First();
             }
             solution.Add(node);
         }
-        static void FindHamilton(int i, ref List<int>[] cominglist, ref List<int> solution, ref int[] visited)
+        static void FindHamilton(int i, ref List<int>[] cominglist, ref List<int> solution, ref int[] visited, ref bool ham)
         {
+            if (ham) return;
             visited[i] = 1;
-            if ((solution.Count ==visited.Length-1) && (cominglist[i].Contains(solution[0])))
+            if ((solution.Count == visited.Length - 1) && (cominglist[i].Contains(solution[0]))) //pierwszy warunek na pewno daje sciezke, drugi dodatkowo daje cykl
             {
-            //   Console.Write("Cykl\n");
+                ham = true;
+              
                 return; //jeśli istnieje krawędź miedzy i a solution[0] to przerywamy- mamy cykl
 
             }
-            if (solution.Count == visited.Length - 1)
-            {
-               // Console.WriteLine("Sciezka\n");
-             //   return; //jeśli licznik=n, znaleźliśmy ścieżkę
-             
-            }
+
             solution.Add(i); //zamiast tablicy stosujemy liste- dodajemy do niej wierzcholek
-            foreach(int j in cominglist[i]) //dla kazdego nastepnika wierzcholka
+            foreach (int j in cominglist[i]) //dla kazdego nastepnika wierzcholka
             {
                 if (j >= 0 && visited[j] != 1)
                 {
-                  
-                   FindHamilton(j, ref cominglist, ref solution, ref visited); //jeśli następnika nie było w visited, wywołujemy rekurencyjnie
+
+                    FindHamilton(j, ref cominglist, ref solution, ref visited, ref ham); //jeśli następnika nie było w visited, wywołujemy rekurencyjnie
                 }
             }
 
-            solution.Remove(i); 
-          visited[i] = 0;
+            solution.Remove(i);
+            visited[i] = 0;
             return;
         }
+   
         static void DeleteHamiltonFromMatrix(ref int[,] adjacencyMatrix, int n)
         {
             //wybieramy losowa wierzcholek i sprawiamy, że ma polaczenie z tylko jednym wierzcholkiem
             Random rnd = new Random();
-            int f = rnd.Next(0, n); //wierzcholek odlaczany
-            int s=f;
-                do s = rnd.Next(0, n);
-            while (f == s);  
+            int f = rnd.Next(0, n-1); //wierzcholek odlaczany
+            int x, y;
+            Console.WriteLine(f);
             for(int i=0; i<n;i++)
+
             {
-                if (i == s) adjacencyMatrix[f, i]= 1;
-                else adjacencyMatrix[f, i] = 0;
+                if(adjacencyMatrix[i, f]== 1)
+                {
+                    do
+                    {
+                         x = rnd.Next(0, n - 1);
+                         y = rnd.Next(0, n - 1);
+                    } while (x == i || y == f||x==y);
+                }
+                if (adjacencyMatrix[f, i] == 1)
+                {
+                    do
+                    {
+                        x = rnd.Next(0, n - 1);
+                        y = rnd.Next(0, n - 1);
+                    } while (x == i || y == f || x == y);
+                }
+
                 adjacencyMatrix[i, f] = 0;
+                adjacencyMatrix[f, i] = 0;
             }
    
         }
@@ -178,7 +192,7 @@ namespace AlgorytmyZPowracaniem
             Console.WriteLine("Czesc A\n");
 
             ////Euler
-            for (int x = 100; x <= 160; x+=6)
+            for (int x = 100; x <= 160; x += 6)
             {
                 n = x; // liczba wierzchołków
                 Console.WriteLine("n=" + n + "\n");
@@ -204,6 +218,7 @@ namespace AlgorytmyZPowracaniem
 
                 // --- Algorytm szukania ---
                 List<int> solution = new List<int>(); // Lista wynikowa
+                sw.Reset();
                 sw.Start();
 
                 FindEuler(0, ref commingList30, ref solution); // funkcja szukająca ciągu Eulera
@@ -220,10 +235,11 @@ namespace AlgorytmyZPowracaniem
                 //   Console.Read();
             }
             //Hamilton
-            for (int x = 1; x <= 10; x++)
+            bool ham = false;
+            for (int x = 1; x <= 4; x++)
             {
-                n = x*5 ;
-               // n=Console.Read();
+                n = 0;
+                n = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("n=" + n + "\n");
                 int m30 = (n * (n - 1) / 2) * 30 / 100; // ilość krawędzi dla nasycenia 30%
                 int[,] adjacencyMatrix30 = new int[n, n]; // macierz sąsiedztwa 30%
@@ -232,10 +248,7 @@ namespace AlgorytmyZPowracaniem
                 int m70 = (n * (n - 1) / 2) * 70 / 100; // ilość krawędzi dla nasycenia 70%
                 int[,] adjacencyMatrix70 = new int[n, n]; // macierz sąsiedztwa 70%
                 GenerateAdjecencyMatrix(ref adjacencyMatrix70, m70, n); // 70%
-                // -- Hamilton --
-                // Mój algorytm wali konia i usuwa krawędzie w listach, niżej masz "kopie" tych list (Usuń "walić konia" bo jak Doktor zobaczy, to bedzie bardziej przypałowo, niż 23:59 xd
-                // Korzystaj z commingList30_Hamilton i commingList70_Hamilton
-                // Gdy graf się generuje za duługo to odpal jeszcze raz. Kwestia losowania, które może zablokować się na amen
+
 
                 List<int>[] commingList30_Hamilton = new List<int>[n]; // Lista następników <- dla Kamila
                 for (int i = 0; i < n; i++) commingList30_Hamilton[i] = new List<int>(); // inicjowanie obiektu
@@ -245,51 +258,53 @@ namespace AlgorytmyZPowracaniem
                 List<int>[] commingList70_Hamilton = new List<int>[n]; // Lista następników <- dla Kamila
                 for (int i = 0; i < n; i++) commingList70_Hamilton[i] = new List<int>(); // inicjowanie obiektu
                 TransformToCommingList(ref adjacencyMatrix70, ref commingList70_Hamilton, n); // 30%
-                                                                                              //tworzenie tablicy visited
+                                                                                              //  tworzenie tablicy visited
                 int[] visited = new int[n];
                 List<int> solution = new List<int>(); // Lista wynikowa
                 sw.Reset();
                 sw.Restart();
-                FindHamilton(0, ref commingList30_Hamilton, ref solution, ref visited); // funkcja szukająca ciągu Hamiltona
+                FindHamilton(0, ref commingList30_Hamilton, ref solution, ref visited, ref ham); // funkcja szukająca ciągu Hamiltona
                 sw.Stop();
                 solution.Clear();
                 Console.WriteLine("Hamilton\t30%\t" + sw.ElapsedMilliseconds + "\n");
                 visited = new int[n];
                 sw.Reset();
+                ham = false;
                 sw.Start();
-                FindHamilton(0, ref commingList70_Hamilton, ref solution, ref visited);
+                FindHamilton(0, ref commingList70_Hamilton, ref solution, ref visited, ref ham);
                 sw.Stop();
                 Console.WriteLine("Hamilton\t70%\t" + sw.ElapsedMilliseconds + "\n");
                 solution.Clear();
 
             }
-            //Sprawdzenie Hamiltona w grafie, w którym nie istnieje cykl hamiltona
-            Console.WriteLine("Czesc B\n");
-             n = 20;
-            int m50 = (n * (n - 1) / 2) * 50 / 100; // ilość krawędzi dla nasycenia 50%
-            int[,] adjacencyMatrix50 = new int[n, n]; // macierz sąsiedztwa 50%
-            GenerateAdjecencyMatrix(ref adjacencyMatrix50, m50, n); // 50%
-            DeleteHamiltonFromMatrix(ref adjacencyMatrix50, n); //usuwamy cykl hamiltona z grafu
-            List<int>[] commingList50_Hamilton = new List<int>[n]; // Lista następników <- dla Kamila
-            for (int i = 0; i < n; i++) commingList50_Hamilton[i] = new List<int>(); // inicjowanie obiektu
-            TransformToCommingList(ref adjacencyMatrix50, ref commingList50_Hamilton, n);
-            int[] Visited = new int[n];
-            List<int> Solution = new List<int>(); // Lista wynikowa
+           // Sprawdzenie Hamiltona w grafie, w którym nie istnieje cykl hamiltona
+            //Console.WriteLine("Czesc B\n");
+            //ham = false;
+            //n = 20;
+            //int m50 = (n * (n - 1) / 2) * 50 / 100; // ilość krawędzi dla nasycenia 50%
+            //int[,] adjacencyMatrix50 = new int[n, n]; // macierz sąsiedztwa 50%
+            //GenerateAdjecencyMatrix(ref adjacencyMatrix50, m50, n); // 50%
+            //DeleteHamiltonFromMatrix(ref adjacencyMatrix50, n); //usuwamy cykl hamiltona z grafu
+            //List<int>[] commingList50_Hamilton = new List<int>[n]; // Lista następników <- dla Kamila
+            //for (int i = 0; i < n; i++) commingList50_Hamilton[i] = new List<int>(); // inicjowanie obiektu
+            //TransformToCommingList(ref adjacencyMatrix50, ref commingList50_Hamilton, n);
+            //int[] Visited = new int[n];
+            //List<int> Solution = new List<int>(); // Lista wynikowa
 
-            for(int i=0;i<commingList50_Hamilton.Count();i++)
-            {
-                foreach(int e in commingList50_Hamilton[i])
-                {
-                    Console.Write("->" + e);
-                }
-                Console.Write("\n");
-            }
-            Console.Write("\n");
-            sw.Reset();
-            sw.Start();
-            FindHamilton(0, ref commingList50_Hamilton, ref Solution, ref Visited);
-            sw.Stop();
-            Console.WriteLine("n="+n+";Hamilton bez cyklu Hamiltona:" + sw.ElapsedMilliseconds + "\n");
+            //for (int i = 0; i < 20; i++)
+            //{
+            //    for (int j = 0; j < 20; j++)
+            //    {
+            //        Console.Write(adjacencyMatrix50[i, j] + ",");
+            //    }
+            //    Console.Write("\n");
+            //}
+            //Console.Write("\n");
+            //sw.Reset();
+            //sw.Start();
+            //FindHamilton(0, ref commingList50_Hamilton, ref Solution, ref Visited, ref ham);
+            //sw.Stop();
+            //Console.WriteLine("n=" + n + ";Hamilton bez cyklu Hamiltona:" + sw.ElapsedMilliseconds + "\n");
             //Console.SetOut(oldOut);
             //writer.Close();
             //ostrm.Close();
